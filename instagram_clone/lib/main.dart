227ea2d5138577d0421b632e380314b384e47655
GenuineCreators <benjamin.mbeva@strathmore.edu>
2024-02-01 01:37:32 +0300

    signUp page

--- instagram_clone/lib/main.dart
+++ instagram_clone/lib/main.dart
@@ -1,12 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/pages/splash_screen.dart';
 
 // ignore: unused_import
 import 'pages/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
---
+++ instagram_clone/lib/services/firebase_auth.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: unused_field
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FireBaseAuthService {
+  // ignore: prefer_final_fields
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // ignore: body_might_complete_normally_nullable
+  Future<User?> signUpWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return credential.user;
+    } catch (e) {
+      print("Some error occured");
+    }
+    return null;
+  }
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential credential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return credential.user;
+    } catch (e) {
+      print("Some error occured");
+    }
+    return null;
+  }
+}
