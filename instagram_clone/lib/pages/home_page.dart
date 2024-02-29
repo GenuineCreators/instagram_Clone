ffb92e974088a51510d7c406b194b8a4c2d1f599
GenuineCreators <benjamin.mbeva@strathmore.edu>
2024-02-29 14:42:59 +0300

    Feeds Page

--- instagram_clone/lib/pages/home_page.dart
+++ instagram_clone/lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -10,20 +10,114 @@ class HomePage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 18),
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Instagram'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.favorite_border_outlined),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.message_rounded),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Instagram'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.favorite_border_outlined),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.message_rounded),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 12,
+                            backgroundImage: NetworkImage(
+                                'https://images.unsplash.com/photo-1457449940276-e8deed18bfff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D'),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                "Username",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.more_horiz),
+                            onPressed: () {},
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.35,
+                        width: double.infinity,
+                        child: Image.network(
+                          'https://images.unsplash.com/photo-1708453104860-ba8f4ea20680?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fE04alZiTGJUUndzfHxlbnwwfHx8fHw%3D',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.comment_outlined,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.send,
+                              )),
+                          Expanded(
+                              child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.bookmark_border)),
+                          ))
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "22000 likes",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                            Row(
+                              children: [
+                                Text(
+                                  "username",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Text("This is a very beautiful photo"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              })),
     );
   }
 }
